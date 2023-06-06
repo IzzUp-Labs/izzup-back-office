4efbd7ac15e413ae7903b714af18116cf6ad8805
Damien <da.fonseca.damien@gmail.com>
2023-06-06 21:33:24 +0200

    :heavy_plus_sign: [ADD] Axios, Pinia, vue-router and Tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      'tablet': '640px',
+      'laptop': '1024px',
+      'desktop': '1280px',
+    },
+    colors: {
+      'izzup-primary': '#00B096',
+      'izzup-secondary': '#0081A7',
+      'izzup-gray': '#A5A5A5',
+      'izzup-black': '#1C1C1E',
+      'izzup-white': '#EDF0EB',
+    },
+    fontFamily: {
+      'sans': ['Roboto', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
