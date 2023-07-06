a4b7fcc3d9a7a7175973597187803eabaac3fcdb
Damien <da.fonseca.damien@gmail.com>
2023-07-06 15:12:53 +0200

    Remove rewrite rules

--- vite.config.ts
+++ vite.config.ts
@@ -8,9 +8,9 @@ export default defineConfig({
     proxy:{
         '/api':{
             target: process.env.VITE_API_URL,
-            ws: true,
-            changeOrigin: true,
-            rewrite: (path) => path.replace(/^\/api/, '')
+            // ws: true,
+            // changeOrigin: true,
+            // rewrite: (path) => path.replace(/^\/api/, '')
         }
     }
   },
