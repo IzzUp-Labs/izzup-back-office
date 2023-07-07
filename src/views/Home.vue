7c2cb830ba7677a672884f5867b82d73ee6c7353
Damien Da Fonseca <da.fonseca.damien@gmail.com>
2023-07-07 02:35:20 +0200

    :ambulance: [UPD] Change route project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,7 +17,7 @@
         <h2 class="card-title">IzzUp Trello</h2>
         <p>La page permettant de gérer l'ensemble des projets izzup !</p>
         <div class="card-actions justify-end">
-          <button class="btn" onclick="location.href = 'https://www.figma.com/file/Tt2F12wskI0VwKFd05IG1O/Register-%2F-Login-Funnel?type=design&node-id=0-1&mode=design&t=IRu8wHuhB6lxYa8r-0'">Y aller</button>
+          <button class="btn" onclick="location.href = 'https://trello.com/w/izzup189'">Y aller</button>
         </div>
       </div>
     </div>
@@ -26,7 +26,7 @@
         <h2 class="card-title">IzzUp Figma</h2>
         <p>Retrouvez l'ensemble de la charte graphique de l'application sur le figma !</p>
         <div class="card-actions justify-end">
-          <button class="btn" onclick="location.href = 'https://trello.com/w/izzup189'">Y aller</button>
+          <button class="btn" onclick="location.href = 'https://www.figma.com/file/Tt2F12wskI0VwKFd05IG1O/Register-%2F-Login-Funnel?type=design&node-id=0-1&mode=design&t=IRu8wHuhB6lxYa8r-0'">Y aller</button>
         </div>
       </div>
     </div>
