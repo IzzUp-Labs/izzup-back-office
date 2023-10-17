5961be75b1f40a910b3c128633abadc026c5d587
Damien Da Fonseca <da.fonseca.damien@gmail.com>
2023-10-17 15:36:39 +0200

    :ambulance: [FIX] Company text

--- src/views/CompanyList.vue
+++ src/views/CompanyList.vue
@@ -23,39 +23,44 @@
   <ShowJobOffers v-if="showModal" class="modal" :class="{ 'modal-open': showModal }" :job_offers="jobOffers" @close="showModal = false"></ShowJobOffers>
   <div v-if="companyList.length > 0">
     <div class="overflow-auto pt-10" style="height: 80vh;">
-      <table class="table table-xs">
-        <!-- head -->
-        <thead>
-        <tr class="">
-          <th>#</th>
-          <th>Adresse</th>
-          <th>Nom</th>
-          <th>Place ID</th>
-          <th>Employeur de la société</th>
-          <th>Location</th>
-          <th>Les offres d'emploi</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr class="bg-base-200 hover:bg-izzup-primary hover:text-izzup-black" v-for="company in companyList" :key="company">
-          <th>{{company.id}}</th>
-          <td>{{company.address}}</td>
-          <td>{{company.name}}</td>
-          <td>{{company.place_id}}</td>
-          <td>{{company.employer.user.first_name}} {{company.employer.user.last_name}}</td>
-          <td>
-            <button class="btn btn-outline">
-              <a :href="`https://www.google.com/maps/place/?q=place_id:${company.place_id}`" target="_blank" rel="noopener noreferrer">Voir sur GoogleMap</a>
-            </button>
-          </td>
-          <td>
-            <button class="btn btn-outline" v-bind:disabled="company?.jobOffers?.length <= 0" v-on:click="openModal(company)">
-              Voir les offres
-            </button>
-          </td>
-        </tr>
-        </tbody>
-      </table>
+      <div class="flex justify-center pb-5">
+        <h2 class="text-3xl font-bold">Liste des sociétées</h2>
+      </div>
+      <div class="overflow-auto" style="height: 80vh;">
+        <table class="table table-xs">
+          <!-- head -->
+          <thead>
+          <tr class="">
+            <th>#</th>
+            <th>Adresse</th>
+            <th>Nom</th>
+            <th>Place ID</th>
+            <th>Employeur de la société</th>
+            <th>Location</th>
+            <th>Les offres d'emploi</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr class="bg-base-200 hover:bg-izzup-primary hover:text-izzup-black" v-for="company in companyList" :key="company">
+            <th>{{company.id}}</th>
+            <td>{{company.address}}</td>
+            <td>{{company.name}}</td>
+            <td>{{company.place_id}}</td>
+            <td>{{company.employer.user.first_name}} {{company.employer.user.last_name}}</td>
+            <td>
+              <button class="btn btn-outline">
+                <a :href="`https://www.google.com/maps/place/?q=place_id:${company.place_id}`" target="_blank" rel="noopener noreferrer">Voir sur GoogleMap</a>
+              </button>
+            </td>
+            <td>
+              <button class="btn btn-outline" v-bind:disabled="company?.jobOffers?.length <= 0" v-on:click="openModal(company)">
+                Voir les offres
+              </button>
+            </td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </template>
